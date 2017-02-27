78f9ba4c79e5a35f4935fea4f7103b2cb3a384e3
nelani <nelani19@gmail.com>
2017-02-26 19:23:31 -0500

    end of 149

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -53,9 +53,12 @@ class ViewController: UIViewController {
     func animateLabelTransitions(){
         
         //Animate the alpha
-        UIView.animate(withDuration: 0.5, animations:{
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
             self.currentQuestionLabel.alpha = 0
             self.nextQuestionLabel.alpha = 1
+        },
+            completion: { _ in
+                swap (&self.currentQuestionLabel, &self.nextQuestionLabel)
         })
     }
     

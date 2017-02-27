056426d3eaae0d165718ca4cb8649026c2debc40
nelani <nelani19@gmail.com>
2017-02-26 20:15:27 -0500

    No bronze or silver

--- Quiz/ViewController.swift
+++ Quiz/ViewController.swift
@@ -70,7 +70,7 @@ class ViewController: UIViewController {
         self.nextQuestionLabelCenterXConstraint.constant = 0
         self.currentQuestionLabelCenterXConstraint.constant += screenWidth
         
-        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveLinear], animations: {
             self.currentQuestionLabel.alpha = 0
             self.nextQuestionLabel.alpha = 1
             
